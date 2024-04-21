c3e57a4d32e25c9849ec3e9eb591c78a8982efb9
daishi <daishi@axlight.com>
2024-04-21 15:19:49 +0900

    wip: eslint 9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,13 +3,19 @@
 ## [Unreleased]
 
 ## [0.0.3] - 2024-04-12
+
 ### Changed
+
 - Compatibility with SSR again
 
 ## [0.0.2] - 2024-04-11
+
 ### Changed
+
 - Compatibility with SSR
 
 ## [0.0.1] - 2024-04-10
+
 ### Added
+
 - Initial release
---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+);
